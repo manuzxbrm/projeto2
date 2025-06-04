f82555b6b5149864d8d8d1307b8d752d896132f1
manuzxbrm <148786839+manuzxbrm@users.noreply.github.com>
2025-06-04 17:36:00 -0300

    h

--- main.js
+++ main.js
@@ -73,5 +73,5 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = Math.floor(2**entropia/(100e6*60*60*24));
+    valorEntropia.textContent = "Um computador pode levar até " + Math.floor(2**entropia/(100e6*60*60*24)) + " dias para descobrir essa senha.";
 }
