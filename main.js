a60eb7b5d7ab3990aff22aee8a93a18481b64d39
manuzxbrm <148786839+manuzxbrm@users.noreply.github.com>
2025-06-03 22:21:10 -0300

    5

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-const numeroSenha = document.querySelector('.parametro-senha__texto');
+const numeroSenha = document.querySelector('.parametro-senha__texto');Adicionar comentárioMore actions
 let tamanhoSenha = 12;
 numeroSenha.textContent = tamanhoSenha;
 
-const botoes = document.querySelectorAll('.parametro-senha__texto');
+const botoes = document.querySelectorAll('.parametro-senha__botao');
 
 botoes[0].onclick = diminuiTamanho;
 botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho() {
     if (tamanhoSenha > 1) {
-       // tamanhoSenha = tamanhoSenha-1;
+        // tamanhoSenha = tamanhoSenha-1;
         tamanhoSenha--;
-     }
-numeroSenha.textContent = tamanhoSenha;
+    }
+    numeroSenha.textContent = tamanhoSenha;
 }
 function aumentaTamanho() {
     if (tamanhoSenha < 20) {
-       // tamanhoSenha = tamanhoSenha+1;
+        // tamanhoSenha = tamanhoSenha+1;
         tamanhoSenha++;
-     }
+    }
     numeroSenha.textContent = tamanhoSenha;
 }
 
