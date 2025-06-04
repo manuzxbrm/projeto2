3b57c43f440495195d918d1819ea42dfd0dcd1f8
manuzxbrm <148786839+manuzxbrm@users.noreply.github.com>
2025-06-03 22:00:12 -0300

    h

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
 const numeroSenha = document.querySelector('.parametro-senha__texto');
 
 numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro-senha__texto');
+
+console.log(botoes);
