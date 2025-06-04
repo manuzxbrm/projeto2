3ba99f2643307d1784b8538c4cf826024da49948
manuzxbrm <148786839+manuzxbrm@users.noreply.github.com>
2025-06-04 17:30:33 -0300

    n

--- main.js
+++ main.js
@@ -73,5 +73,5 @@ function classificaSenha(tamanhoAlfabeto){
         forcaSenha.classList.add('fraca');
     }
     const valorEntropia = document.querySelector('.entropia');
-    valorEntropia.textContent = 2**Math.floor(entropia);
+    valorEntropia.textContent = 2**Math.floor(entropia)/(100e6*60*60*24);
 }
